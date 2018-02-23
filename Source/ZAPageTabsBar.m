89d0ca94648ca189e26bd6ac26f89280dc29df93
ZartArn <nlevch@yandex.ru>
2018-02-24 01:30:48 +0300

    fix: calculating with of cell for pagebar

--- Source/ZAPageTabsBar.m
+++ Source/ZAPageTabsBar.m
@@ -85,33 +85,27 @@
 
 - (void)calculateWidths {
     NSMutableDictionary *cached = [NSMutableDictionary dictionaryWithCapacity:self.items.count];
+    
+    ZAPageTabsCell *tmpCell = [[ZAPageTabsCell alloc] initWithFrame:CGRectZero];
+    UIFont *titleFont = tmpCell.titleLabel.font;
+    
     [self.items enumerateObjectsUsingBlock:^(NSString *title, NSUInteger idx, BOOL * _Nonnull stop) {
-//        NSAttributedString *s = [[NSAttributedString alloc] initWithString:title
-//                                                                attributes:@{
-//                                                                             NSFontAttributeName : [UIFont systemFontOfSize:16.f]
-//                                                                             }];
-//        CGSize titleSize = [title sizeWithAttributes:@{NSFontAttributeName:[Typography regularFontWithSize:16.f]}];
         
         NSAttributedString *ss =[[NSAttributedString alloc] initWithString:title
                                                                 attributes: @{
-                                                                              NSFontAttributeName : [Typography regularFontWithSize:18.f]
+                                                                              NSFontAttributeName : titleFont
                                                                               }];
         
-        CGRect titleRect = [ss boundingRectWithSize:(CGSize){CGFLOAT_MAX, 50.f}
+        CGRect titleRect = [ss boundingRectWithSize:(CGSize){CGFLOAT_MAX, 44.f}
                                                options:NSStringDrawingUsesFontLeading|NSStringDrawingUsesLineFragmentOrigin
                                                context:nil];
         
-//        CGSize titleSize = [title boundingRectWithSize:(CGSize){INFINITY, INFINITY}
-//                                               options:NSStringDrawingOptions|NSStringDrawingUsesLineFragmentOrigin
-//                                            attributes:attributes:@{
-//                                                                    NSFontAttributeName : [UIFont systemFontOfSize:16.f]
-//                                                                    }
-//                                           context:nil];
         double w = ceil(titleRect.size.width) + TITLE_INSETS.left + TITLE_INSETS.right;
         [cached setObject:@(w) forKey:[NSIndexPath indexPathForRow:idx inSection:0]];
     }];
+    
     self.cachedCellWidths = [cached copy];
-//    NSLog(@"%@", self.cachedCellWidths);
+    NSLog(@"%@", self.cachedCellWidths);
 }
 
 #pragma mark - UICollectionViewDataSource
@@ -136,25 +130,35 @@
     cell.icon.highlightedImage = [StyleHelper loadImage:icon];
     cell.icon.image = nil;
     
-//    [cell configureWithViewModel:[self.viewModel cellViewModelForIndexPath:indexPath]];
 }
 
 #pragma mark - UICollectionViewDelegateFlowLayout
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     
-    CGFloat allWidths = [[[self.cachedCellWidths allValues] valueForKeyPath:@"@sum.self"] doubleValue];
+    NSArray *cachedWidthValues = [self.cachedCellWidths allValues];
+    CGFloat allWidths = [[cachedWidthValues valueForKeyPath:@"@sum.self"] doubleValue];
     
     if (allWidths > collectionView.bounds.size.width) {
         return (CGSize){[self.cachedCellWidths[indexPath] doubleValue], collectionView.bounds.size.height};
     }
     
+    CGFloat maxWidth = [[cachedWidthValues valueForKeyPath:@"@max.self"] doubleValue];
     CGFloat w = (collectionView.bounds.size.width / self.items.count);
-    CGSize size = (CGSize){w, collectionView.bounds.size.height};
+    if (w > maxWidth) {
+        return (CGSize){w, collectionView.bounds.size.height};
+    }
     
-    NSLog(@"collectionView size :: %@", NSStringFromCGSize(collectionView.frame.size));
-    NSLog(@"cell size :: %@", NSStringFromCGSize(size));
-    NSLog(@"\n\n");
+    // streched widths
+    CGFloat addSpacing = (collectionView.bounds.size.width - allWidths) / (float)self.items.count;
+    CGSize size = (CGSize){
+                        [self.cachedCellWidths[indexPath] doubleValue] + addSpacing,
+                        collectionView.bounds.size.height
+                    };
+//
+//    NSLog(@"collectionView size :: %@", NSStringFromCGSize(collectionView.frame.size));
+//    NSLog(@"cell size :: %@", NSStringFromCGSize(size));
+//    NSLog(@"\n\n");
     
     return size;
 }
